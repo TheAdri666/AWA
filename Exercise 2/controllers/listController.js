9276f5041aec14f42ad81c70c33592e764499a44
TheAdri666 <adrianinfante66@gmail.com>
2022-11-13 15:09:43 +0200

    Exercise 2 nOdE vErSIon 12

--- Exercise 2/controllers/listController.js
+++ Exercise 2/controllers/listController.js
@@ -1,11 +1,12 @@
 let wordList = [];
 
 function addToList(req, res) {
-  const text = req?.body?.text;
-  if(text) {
-    wordList.push(text);
-    return res.status(200).send({"list": wordList});
+  if(!req || !req.body || !req.body.text) {
+    return res.status(400);
   }
+  const text = req.body.text;
+  wordList.push(text);
+  return res.status(200).send({"list": wordList});
 }
 
 module.exports = {
--- Exercise 2/controllers/sumController.js
+++ Exercise 2/controllers/sumController.js
@@ -1,11 +1,12 @@
 function sum(req, res) {
   let sum = 0;
-  const numbers = req?.body?.numbers;
-  if(numbers) {
-    for (const char of numbers) {
-      if (char !== " " && !isNaN(char)) {
-        sum += parseInt(char);
-      }
+  if(!req || !req.body || !req.body.numbers) {
+    return res.status(400);
+  }
+  const numbers = req.body.numbers;
+  for (const char of numbers) {
+    if (char !== " " && !isNaN(char)) {
+      sum += parseInt(char);
     }
   }
   return res.status(200).send({"sum": sum});
